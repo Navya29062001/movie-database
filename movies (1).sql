11235e5045c35193f11b1cf9c29db6aecae97c97
Navya29062001 <104219442+Navya29062001@users.noreply.github.com>
2022-05-11 21:16:44 +0530

    Add files via upload

---
+++ movies (1).sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2022 at 05:33 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movies db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `Movie_name` varchar(20) NOT NULL,
+  `Actor` varchar(25) NOT NULL,
+  `Actress` varchar(25) NOT NULL,
+  `Director_name` varchar(25) NOT NULL,
+  `Release_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movies`
+--
+
+INSERT INTO `movies` (`Movie_name`, `Actor`, `Actress`, `Director_name`, `Release_date`) VALUES
+('KGF', 'Yash', 'Srinidhi shetty', 'Prashanth neel', '2022-04-14'),
+('James', 'PuneethRajkumar', 'Priya Wajanand', 'chetan kumar', '2022-03-17'),
+('Beast', 'Vijay', 'Pooja hedge', 'Dilipkumar', '2022-04-13'),
+('Charlie777', 'Rakshith Shetty', 'Sangeetha', 'Rishab Shetty', '2022-05-09'),
+('Baahubali', 'Prabhas', 'Anushka', 'Rajmouli', '2020-04-24');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
